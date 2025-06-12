44d475a472af95b740cbac3a1c4dc822e0563cc0
Kavinda Perera <kavinda_2020675@fot.ruh.ac.lk>
2025-06-12 21:28:44 +0530

    db configration

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.DB_PWD,
+    databse: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+
+module.exports = pool;
