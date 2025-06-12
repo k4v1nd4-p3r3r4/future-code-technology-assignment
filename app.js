d812d4c06f18b7362c4f8b8484dde893184c4a45
Kavinda Perera <kavinda_2020675@fot.ruh.ac.lk>
2025-06-12 22:08:21 +0530

    file structure setup

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
