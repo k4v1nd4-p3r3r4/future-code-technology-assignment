9411e11a10890f75010314f1a08ede15d726d075
Kavinda Perera <kavinda_2020675@fot.ruh.ac.lk>
2025-06-13 02:09:54 +0530

    add hash function to password

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,11 @@
 const pool = require('../config/db');
+const bcrypt = require('bcrypt');
 
-const createUser = async (name,username,password) => {
+const createUser = async (name, username, password) => {
+    const hashedPassword = await bcrypt.hash(password, 10);
     const [result] = await pool.execute(
         'INSERT INTO users (name, username, password) VALUES (?,?,?)',
-        [name,username, password]
+        [name, username, hashedPassword]
     );
     return result.insertId;
 };
@@ -17,12 +19,10 @@ const findUserByUsername = async (username) => {
 };
 
 const findByCredentials = async (username, password) => {
-    const [rows] = await pool.execute(
-        'SELECT * FROM users WHERE username = ? AND password = ?',
-        [username, password]
-    );
-    return rows[0];
+    const user = await findUserByUsername(username);
+    if (!user) return null;
+    const isMatch = await bcrypt.compare(password, user.password);
+    return isMatch ? user : null;
 };
-    
 
-module.exports = {createUser, findUserByUsername, findByCredentials};
+module.exports = { createUser, findUserByUsername, findByCredentials };
