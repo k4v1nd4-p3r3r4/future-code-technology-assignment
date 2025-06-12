3eb7f0ddcdc0593b678248b1d413472c60e89ad6
Kavinda Perera <kavinda_2020675@fot.ruh.ac.lk>
2025-06-12 22:01:58 +0530

    create sql table

---
+++ product_api.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE product_db;
+
+use product_api;
+
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE products(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
